7165845457b26333f361eefa0a06d55f7b215a44
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-20 00:37:52 +0900

    #1. JsonWebsocketConsumer 적용

--- app/consumers.py
+++ app/consumers.py
@@ -1,30 +1,28 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class LiveblogConsumer(WebsocketConsumer):
+class LiveblogConsumer(JsonWebsocketConsumer):
     groups = ["liveblog"]
 
     def liveblog_post_created(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(json.dumps(event_dict))
 
     def liveblog_post_updated(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(json.dumps(event_dict))
 
     def liveblog_post_deleted(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(json.dumps(event_dict))
 
 
-class EchoConsumer(WebsocketConsumer):
-    def receive(self, text_data=None, bytes_data=None):
-        obj = json.loads(text_data)
-        print(f"수신: {obj}")
+class EchoConsumer(JsonWebsocketConsumer):
+    def receive_json(self, content, **kwargs):
+        print(f"수신: {content}")
 
-        json_string = json.dumps(
+        self.send_json(
             {
-                "content": obj["content"],
-                "user": obj["user"],
+                "content": content["content"],
+                "user": content["user"],
             }
         )
-        self.send(json_string)
