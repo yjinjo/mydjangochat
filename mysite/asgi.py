13f15890ec03a685c6018acf16c8500dcbdc32b3
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-20 15:44:01 +0900

    #1. 웹 소켓 CSRF 보안

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import app.routing
@@ -24,9 +25,12 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                app.routing.websocket_urlpatterns + chat.routing.websocket_urlpatterns
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    app.routing.websocket_urlpatterns
+                    + chat.routing.websocket_urlpatterns
+                )
             )
         ),
     }
