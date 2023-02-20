8a2112fd1119fa7c50b9d93ba23df7504c263147
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-20 15:36:14 +0900

    #1. 채팅방 참여자 목록 노출

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from chat.models import Room
+from chat.models import Room, RoomMember
 
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RoomMember)
+class RoomMemberAdmin(admin.ModelAdmin):
     pass
